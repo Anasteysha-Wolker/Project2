4c6195a24a7d751ab64856992e60fcb2ab3360ee
Anastasia Dovbenco <61821066+Anasteysha-Wolker@users.noreply.github.com>
2020-05-05 16:19:15 +0300

    Update User.java

--- src/pack1/User.java
+++ src/pack1/User.java
@@ -26,6 +26,18 @@ public class User implements Comparable<User> {
     public void setAccess(boolean access) { this.access = access; }
 
 
+    public static void bubbleSort(int[] arr){
+
+        for(int i = arr.length-1 ; i > 0 ; i--){
+            for(int j = 0 ; j < i ; j++){
+
+            if( arr[j] > arr[j+1] ) {
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 
     @Override
     public int compareTo(User o) {
